b925161b3112bffde9f96425c21db0a11f5ca8a8
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2017-02-22 15:09:58 +0900

    Added train_model()

--- model.py
+++ model.py
@@ -41,7 +41,32 @@ def get_preprocessing_model(image_size):
     return model
 
 
-def get_model(image_size):
+def get_prediction_pipeline(x):
+
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Convolution2D(nb_filter=64, nb_row=3, nb_col=3, activation='elu', border_mode='same')(x)
+    x = keras.layers.Convolution2D(nb_filter=128, nb_row=3, nb_col=3, activation='elu', border_mode='same')(x)
+    x = keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Convolution2D(nb_filter=64, nb_row=3, nb_col=3, activation='elu', border_mode='same')(x)
+    x = keras.layers.Convolution2D(nb_filter=128, nb_row=3, nb_col=3, activation='elu', border_mode='same')(x)
+    x = keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Convolution2D(nb_filter=64, nb_row=3, nb_col=3, activation='elu', border_mode='same')(x)
+    x = keras.layers.Convolution2D(nb_filter=128, nb_row=3, nb_col=3, activation='elu', border_mode='same')(x)
+    x = keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(output_dim=1)(x)
+
+    return x
+
+
+def get_model(image_size, save_path):
+
+    callbacks = [keras.callbacks.ModelCheckpoint(filepath=save_path, verbose=1, save_best_only=True)]
 
     expected_image_size = (160, 320, 3)
 
@@ -50,8 +75,10 @@ def get_model(image_size):
 
     input = keras.layers.Input(shape=image_size)
     x = get_preprocessing_pipeline(input)
+    x = get_prediction_pipeline(x)
 
     model = keras.models.Model(input=input, output=x)
+    model.compile(optimizer='adam', loss='mean_squared_error', callbacks=callbacks)
     return model
 
 
@@ -104,11 +131,29 @@ def get_single_dataset_generator(csv_path, minimum_angle=0):
             yield image, steering_angle
 
 
+def get_dataset_samples_count(csv_path, minimum_angle):
+
+    with open(csv_path) as file:
+
+        reader = csv.reader(file)
+        csv_lines = [line for line in reader if abs(float(line[3])) >= minimum_angle]
+
+    return len(csv_lines)
+
+
 def get_multiple_datasets_generator(paths, minimum_angles, batch_size):
+    """
+    Generator that cycles through multiple datasets
+    :param paths: paths to drive log csv files
+    :param minimum_angles: minimum angles frame from corresponding data set must have to be outputted
+    :param batch_size: batch size
+    :return: generator that yields images, steering_angles batches.
+    """
 
     generators = [get_single_dataset_generator(path, minimum_angle)
                   for path, minimum_angle in zip(paths, minimum_angles)]
 
+    # We will cycle through different generators, so that each dataset is sampled evenly
     generators_cycle = itertools.cycle(generators)
 
     while True:
@@ -124,6 +169,33 @@ def get_multiple_datasets_generator(paths, minimum_angles, batch_size):
             images.append(image)
             steering_angles.append(angle)
 
-        yield images, steering_angles
+        yield np.array(images), np.array(steering_angles)
 
 
+def train_model():
+
+    parent_dir = "../../data/behavioral_cloning/"
+
+    paths = [
+        "track_1_center/driving_log.csv",
+        "track_2_center/driving_log.csv",
+        "track_1_curves/driving_log.csv",
+        "track_2_curves/driving_log.csv",
+        "track_1_recovery/driving_log.csv",
+        "track_2_recovery/driving_log.csv"
+    ]
+
+    paths = [os.path.join(parent_dir, path) for path in paths]
+    angles = [0, 0, 0.02, 0.02, 0.1, 0.1]
+
+    generator = get_multiple_datasets_generator(paths, angles, batch_size=32)
+
+    samples_count = sum([get_dataset_samples_count(path, minimum_angle) for path, minimum_angle in zip(paths, angles)])
+
+    model = get_model(image_size=(160, 320, 3), save_path="./model.h5")
+    model.fit_generator(generator, samples_per_epoch=samples_count, nb_epoch=10)
+
+
+if __name__ == "__main__":
+
+    train_model()
--- visualization.py
+++ visualization.py
@@ -7,6 +7,7 @@ import logging
 
 import vlogging
 import cv2
+import numpy as np
 
 import model
 
@@ -65,7 +66,38 @@ def log_all_datasets_generator_output(logger):
         # Resize images slightly
         images = [cv2.pyrDown(image) for image in images]
 
-        logger.info(vlogging.VisualRecord("Frame", images, "Steering angle: {}".format(steering_angles)))
+        logger.info(vlogging.VisualRecord(
+            "Frames", images, "Steering angle: {}".format(steering_angles.tolist())))
+
+
+def log_preprocessed_datasets_generator_output(logger):
+
+    parent_dir = "../../data/behavioral_cloning/"
+
+    paths = [
+        "track_1_center/driving_log.csv",
+        "track_2_center/driving_log.csv",
+        "track_1_curves/driving_log.csv",
+        "track_2_curves/driving_log.csv",
+        "track_1_recovery/driving_log.csv",
+        "track_2_recovery/driving_log.csv"
+    ]
+
+    paths = [os.path.join(parent_dir, path) for path in paths]
+    angles = [0, 0, 0.02, 0.02, 0.1, 0.1]
+
+    generator = model.get_multiple_datasets_generator(paths, angles, batch_size=6)
+    preprocessing_model = model.get_preprocessing_model(image_size=(160, 320, 3))
+
+    for _ in range(10):
+
+        images, steering_angles = next(generator)
+
+        preprocessed_images = preprocessing_model.predict(images)
+        preprocessed_images = [(255 * image).astype(np.uint8) for image in preprocessed_images]
+
+        logger.info(vlogging.VisualRecord(
+            "Frames", preprocessed_images, "Steering angle: {}".format(steering_angles.tolist())))
 
 
 def main():
@@ -73,7 +105,9 @@ def main():
     logger = get_logger("/tmp/behavioral_cloning.html")
 
     # log_single_generator_output(logger)
-    log_all_datasets_generator_output(logger)
+    # log_all_datasets_generator_output(logger)
+    log_preprocessed_datasets_generator_output(logger)
+
 
 
 if __name__ == "__main__":
